2d6b7f6286e735769dc4dfc2810c273600a4f6d3
Irsal Lazuard Izzatillah <91246036+IrsalLaz@users.noreply.github.com>
2023-10-22 01:54:43 +0700

    MovieSearch - add exception for connection issue, store and return movies

--- apps/10_movie_search/MovieSearch.py
+++ apps/10_movie_search/MovieSearch.py
@@ -1,4 +1,9 @@
 import requests
+import collections
+
+MovieResult = collections.namedtuple(
+    'MovieResult',
+    "imdb_code,title,duration,director,year,rating,imdb_score,keywords,genres")
 
 
 def main():
@@ -10,31 +15,50 @@ def main():
 
 
 def search_loop():
-    title = ""
+    title = "required field"
 
     while title != "x":
         try:
             title = input("Enter movie title(x to exit): ")
 
             if title != "x":
-                find_movies(title)
+                result = find_movies(title)
+                for i in result:
+                    print(f"{i.year} -- {i.title}")
 
-        except ValueError:
+        except ValueError:                                      # handle empty title
             print("Error: title is required")
-
-        except Exception as x:
+        except requests.exceptions.ConnectionError:             # handle connection issue
+            print("Host unreachable, check your network connection!")
+        except Exception as x:                                  # handle other error
             print("Unexpected error. Details: {}".format(x))
 
     print("Exiting the app...")
 
 
 def find_movies(title):
-    url = f'http://movie_service.talkpython.fm/api/search/{title}'
+    # check if its empty string or white spaces
+    if not title or not title.strip():
+        raise ValueError("Title is required")
+
+    # Replace spaces with %20
+    title_encoded = title.replace(" ", "%20")
+    url = f'http://movie_service.talkpython.fm/api/search/{title_encoded}'
 
     response = requests.get(url)
     data = response.json()
 
-    print(data)
+    # get movie data that stored in hits object
+    movie_data = data.get("hits")
+
+    #
+    movies = [
+        MovieResult(**md) for md in movie_data
+    ]
+
+    movies.sort(key=lambda m: -m.year)
+
+    return movies
 
 
 if __name__ == '__main__':
