f9f188bb4fe07dad512a19b5ed85b409ad1d81cf
Irsal Lazuard Izzatillah <91246036+IrsalLaz@users.noreply.github.com>
2023-10-22 04:46:11 +0700

    WizardBattle - add simple logic

--- apps/07_wizard_battle/WizardBattle.py
+++ apps/07_wizard_battle/WizardBattle.py
@@ -1,9 +1,38 @@
+import time
+
+
 def main():
     print("-------------------------")
     print("WIZARD BATTLE".center(25, " "))
     print("-------------------------")
 
-    # put the logic game here
+    game_loop()
+
+
+def game_loop():
+    looping = True
+
+    while looping:
+        # put the logic game here
+        print("The wizard {player} sees a {creature}.")
+
+        action = input("Do you [a]ttack, [r]un away, or [l]ook around? ")
+
+        if action == "a":
+            # put the attack logic here
+            print()
+
+        elif action == "r":
+            print("The wizard {player} runs away to finds some cover.")
+            # pause the game and then continue
+            time.sleep(3)
+            print("The wizard {player} continue continue his journey.")
+
+        elif action == "l":
+            print("The wizard {player} look around and sees: ")
+            # print all creature then continue
+        else:
+            break
 
 
 if __name__ == '__main__':
