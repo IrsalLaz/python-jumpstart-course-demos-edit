d3281af14cb996e0847bd0bd19bcdc58289f0b29
Irsal Lazuard Izzatillah <91246036+IrsalLaz@users.noreply.github.com>
2023-10-22 14:48:49 +0700

    WizardBattle - add attack, run, and look logic

--- apps/07_wizard_battle/Actors.py
+++ apps/07_wizard_battle/Actors.py
@@ -1,18 +1,39 @@
+import random
+
+
 class Creature:
     def __init__(self, name, level):
         self.name = name
         self.level = level
 
-    def fight(self, creature):
-        print(f"The {self.name} fights {creature.name}!")
+    def roll_the_dice(self):
+        return random.randint(1, 12) * self.level
 
 
 class Wizard(Creature):
-    print("this is Wizard")
+    def attack(self, creature):
+        print(f"The {self.name} fights {creature.name}!")
+
+        player_role = self.roll_the_dice()
+        creature_role = creature.roll_the_dice()
+
+        print(f"The wizard roles {player_role}")
+        print(f"The {creature.name} roles {creature_role}")
+
+        # rule of the game
+        if player_role >= creature_role:
+            print(f"The wizard has triumphant over the {creature.name}!")
+            return True     # the creature will be removed from the list
+        else:
+            print(f"The wizard {self.name} has been defeated by the {creature.name}")
+            return False   # the player will be defeated
 
 
 class SmallAnimal(Creature):
-    print("this is smallAnimal")
+    # modify the base method so the small animal will be easier to defeat
+    def roll_the_dice(self):
+        small_animal_role = super().roll_the_dice()
+        return small_animal_role / 2
 
 
 class Dragon(Creature):
--- apps/07_wizard_battle/WizardBattle.py
+++ apps/07_wizard_battle/WizardBattle.py
@@ -1,4 +1,6 @@
+import random
 import time
+from Actors import Creature, Wizard, SmallAnimal, Dragon
 
 
 def main():
@@ -12,25 +14,49 @@ def main():
 def game_loop():
     looping = True
 
+    # define the player
+    player = Wizard("Gondalf", 20)
+
+    # define other creatures
+    creatures = [
+        SmallAnimal("Toad", 1),
+        SmallAnimal("Bat", 5),
+        Creature("Tiger", 12),
+        Dragon("Dragon", 50),
+        Wizard("Evil Wizard", 1000)
+    ]
+
     while looping:
-        # put the logic game here
-        print("The wizard {player} sees a {creature}.")
+        # make the creature appear randomly
+        creature = random.choice(creatures)
+        print(f"The wizard {player.name} sees a {creature.name}.")
 
         action = input("Do you [a]ttack, [r]un away, or [l]ook around? ")
 
         if action == "a":
-            # put the attack logic here
+            if player.attack(creature):
+                # if player attack the creature and return true, then the creature will be removed from the list
+                creatures.remove(creature)
+            else:
+                # if the creature win, then game pause for 10 seconds
+                time.sleep(10)
+                print("\nThe wizard respawned with full health!")
             print()
 
         elif action == "r":
-            print("The wizard {player} runs away to finds some cover.")
+            print(f"The wizard {player.name} runs away to finds some cover.")
             # pause the game and then continue
             time.sleep(3)
-            print("The wizard {player} continue continue his journey.")
+            print(f"The wizard {player.name} continue continue his journey.")
+            print()
 
         elif action == "l":
             print("The wizard {player} look around and sees: ")
-            # print all creature then continue
+            # print all remaining creature then continue
+            for creature in creatures:
+                print(f" * A level {creature.level} of {creature.name}.")
+            print()
+
         else:
             break
 
